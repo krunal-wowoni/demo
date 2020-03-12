929a0273b953f06062ad194cef1fb6bf30eb42ea
krunal.wowoni <krunal.wowoni@gmail.com>
2020-03-12 16:17:39 +0530

    added controller

--- LoginApi/src/main/java/com/example/demo/LoginApiApplication.java
+++ LoginApi/src/main/java/com/example/demo/LoginApiApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginApiApplication.class, args);
+	public static void main(String[] arg) {
+		System.out.println("");
+		SpringApplication.run(LoginApiApplication.class, arg);
 	}
 
 }
