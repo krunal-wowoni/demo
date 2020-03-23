35935a865110ae66599df028cbb815e26399b755
krunal.wowoni <krunal.wowoni@gmail.com>
2020-03-23 18:17:59 +0530

    added PostgreSQL Demo

---
+++ PostgreSQL-demo/src/main/java/com/example/postgresdemo/PostgreSqlDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.postgresdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class PostgreSqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSqlDemoApplication.class, args);
+	}
+
+}
